cf8b874c1d5f298f11b21ace154ee9d3f0cefd22
Katarzyna Rdzanek <kkrdzanek@gmail.com>
2022-03-03 19:25:10 +0100

    added missing fileuploads

--- tests/pages/attachments.js
+++ tests/pages/attachments.js
@@ -4,5 +4,13 @@ import path from 'path';
 
 class Attachments extends Page {
 
-    get attachmentButton() { return $('[class="thisClassName"]')}
+    get attachmentButton() { return $('[class="thisClassName"]') }
+
+    async uploadAttachment() {
+        const remoteFilePath = await browser.uploadFile(path.join(__dirname, '../../uploades/file.jpeg'))
+        const input = await this.attachmentButton;
+        await browser.execute(input => input.style.display = 'block', input);
+        input.waitForDisplayed({ timeout: process.env.WAIT_LOW });
+        input.setValue(remoteFilePath);
+    }
 }
